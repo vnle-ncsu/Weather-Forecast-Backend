84c1ddc1fa895e0c6b87994ab564a56f47fa0c38
Aznricerr <victor.nt.le@gmail.com>
2024-07-24 22:37:49 -0500

    activity suit changes

--- forecast_service.py
+++ forecast_service.py
@@ -2,6 +2,7 @@ from forecast_client import ForecastClient
 from models import ForecastRequestModel, ForecastResponseModel, SevenDayForecastResponseModel, CurrentForecastModel, HourlyForecastModel, RatingRequestModel, RatingResponseModel
 import tensorflow as tf
 import numpy as np
+import joblib
 
 
 class ForecastService:
@@ -64,20 +65,33 @@ class ForecastService:
         return interpreter
 
     def get_activity_suitability(self, request: RatingRequestModel) -> int:
+        scaler = joblib.load('scaler.joblib')
+        activity_mapping = {'golf': 1, 'hiking': 2, 'running': 3}
+        
+        
+        input_data_activity = activity_mapping[request.activity]
+
         input_details = self.activity_model.get_input_details()
         output_details = self.activity_model.get_output_details()
 
         #print("Input details:", input_details)
 
-        activity_encoded = np.array([request.activity.encode('utf-8')], dtype=np.string_)
+        #activity_encoded = np.array([request.activity.encode('utf-8')], dtype=np.string_)
+        print(type(input_data_activity))
+        numerical_inputs = np.array([[input_data_activity, request.temp_max, request.precipitation, request.temp_min, request.weather_code]], dtype=np.float32)
+        for i in numerical_inputs:
+            print(type(i))
+        input_scaled = scaler.transform(numerical_inputs)
+        for iss in input_scaled:
+            print(type(iss))
+        self.activity_model.set_tensor(input_details[0]['index'], input_scaled)
 
-        numerical_inputs = np.array([[request.temp_max, request.precipitation, request.temp_min, request.weather_code, 0.0]], dtype=np.float32)
 
-        for input_detail in input_details:
-            if input_detail['dtype'] == np.string_:
-                self.activity_model.set_tensor(input_detail['index'], activity_encoded)
-            elif input_detail['dtype'] == np.float32:
-                self.activity_model.set_tensor(input_detail['index'], numerical_inputs)
+        #for input_detail in input_details:
+        #    if input_detail['dtype'] == np.string_:
+        #        self.activity_model.set_tensor(input_detail['index'], activity_encoded)
+        #    elif input_detail['dtype'] == np.float32:
+        #        self.activity_model.set_tensor(input_detail['index'], numerical_inputs)
 
         self.activity_model.invoke()
         y_pred = self.activity_model.get_tensor(output_details[0]['index'])[0]
